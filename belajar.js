030ef21af91db0420e7d4764d567e85aa8c267e2
Ujang Wahyu <sohibulwapa29@gmail.com>
2025-11-05 11:06:44 +0700

    menambah fitur baru

--- belajar.js
+++ belajar.js
@@ -1,3 +1,56 @@
-console.log("ini aku belajar tentang GIT");
-console.log("Belajar kali ini pake git hub");
-console.log("baru lagi");
+const { resolve } = require("path");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function prompt(pertanyaan, callback){
+    rl.question(pertanyaan, (jawaban) => {
+        callback(jawaban);
+    });
+}
+
+function pilihan(){
+   prompt("Silahkan masukkan Pilihan Anda:\n 1.lihat data\n 2.tambah data\n",function(milih){
+    if(milih === "1"){
+        coba();
+    }else{
+        console.log("blok");
+        rl.close();
+    }
+   });
+
+}
+
+function coba(){
+    console.log("selamat datang");
+    prompt("siapa nama kamu? ", function(nama) {
+        console.log(`halo ${nama}`);
+
+        prompt("apakah ingin melanjutkan?\n 1.iya\n 2.tidak\n", function(pilih){
+            if(pilih === "1"){
+                pilihan();
+            }else{
+                console.log("blok");
+                rl.close();
+            }
+        });
+
+    });
+
+    
+}
+
+
+// var array = [{id: 1, nama: "sohibul wapa", asal: "jampang kulon", lulusan: "MA"},
+//     {id: 2, nama: "sohib", asal: "sukabumi", lulusan: "SMA"},
+//     {id: 3, nama: "keno", asal: "jawa barat", lulusan: "SMK"}
+// ];
+
+// array.forEach(item => {
+//     console.log(`${item.id} | ${item.nama} | ${item.asal} | ${item.lulusan}`);
+//     rl.close();
+// });
+
+pilihan();
